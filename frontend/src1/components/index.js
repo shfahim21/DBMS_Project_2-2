dd38a3262aa870b3bc4ea9f61ab43d3aa986a295
fahim <fahimsh2002@gmail.com>
2025-01-26 00:59:32 +0600

    unused item deleted

--- frontend/src1/components/index.js
+++
@@ -1,9 +0,0 @@
-// src/components/index.js
-export * from './AuthorsSection';
-export * from './CategoriesSection';
-export * from './CustomersSection'; // Note plural form
-export * from './AuthorModal';
-export * from './CategoryModal';
-export * from './CustomerModal';
-export * from './DataTable';
-export * from './SearchBar';
